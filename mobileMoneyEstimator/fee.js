49b9a317d831b28190421278c8d26166a08c2997
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-06-08 15:32:47 +0300

    Work on money estimator solution

---
+++ mobileMoneyEstimator/fee.js
@@ -0,0 +1,23 @@
+function estimateTransactionFee(amoutToSend) {
+    // Sulution
+    
+   
+    
+
+
+console.log(`Sending KES ${amountToSend}`);
+console.log(`Calculated Transaction Fee: KES ${}`);
+console.log(`Total amount to be debited: KES ${}`);
+console.log(`\nSend Money Securely!`);
+}
+
+// Prompt the user for the amount to send
+const inputAmount = prompt("Unatuma Ngapi? (KES):");
+const amount = Number(inputAmount);
+
+// Validate the input and call the function
+if (isNaN(amount) || amount <= 0) {
+    console.log("Please enter a valid amount in Kenyan Shillings (KES).");
+} else {
+
+}
