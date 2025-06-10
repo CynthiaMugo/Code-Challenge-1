df9aa05395f56d812b091f33a12bd01249605644
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-06-10 18:47:55 +0300

    Mobile money estimator solution

--- mobileMoneyEstimator/fee.js
+++ mobileMoneyEstimator/fee.js
@@ -1,11 +1,19 @@
-function estimateTransactionFee(amountToSend) {
-    // Sulution
+function estimateTransactionFee() {
+    const inputAmount = prompt("Unatuma Ngapi? (KES):");
+    const amount = parseInt(inputAmount);
+    // Validate input
+    if (isNaN(amount) || amount <= 0) {
+        alert(`Please enter a valid amount`);
+    return;
+    }
+
+    // Solution
     const transactionFee = 0.015; // 1.5% transaction fee
     const minFee = 10; // Minimum fee in KES
     const maxFee = 70; // Maximum fee in KES
 
     // Calculate the fee based on the amount to send
-    const calculatedFee = amountToSend * transactionFee;
+    const calculatedFee = amount * transactionFee;
 
     // Conditional logic to calculate the fee
     if (calculatedFee < minFee) {
@@ -17,25 +25,16 @@ function estimateTransactionFee(amountToSend) {
     }
 
     // Calculate the total amount to be debited
-    const totalDebited = amountToSend + finalFee;
-   
+    const totalFee = amount + finalFee;
 
-console.log(`Sending KES ${amountToSend}`);
-console.log(`Calculated Transaction Fee: KES ${calculatedFee}`);
-console.log(`Total amount to be debited: KES ${totalDebited}`);
-console.log(`\nSend Money Securely!`);
+    alert(`Sending KES ${amount}
+        Calculated Transaction Fee: KES ${calculatedFee}
+        Total amount to be debited: KES ${totalFee}
+        \nSend Money Securely!
+        `);
+
 }
-
-// Prompt the user for the amount to send
-const inputAmount = prompt("Unatuma Ngapi? (KES):");
-const amount = Number(inputAmount);
-
-// Validate the input and call the function
-if (isNaN(amount) || amount <= 0) {
-    console.log("Please enter a valid amount in Kenyan Shillings (KES).");
-} else {
-    estimateTransactionFee(amount);
-}
+estimateTransactionFee()
 
 // Test with 500, 2000 & 10000
-// Error encountered - using return in the if...else block made my code stop executing after line 17 hence used another variable finalFee
+// Error encountered - using return in the if...else block made my code stop executing after line 20 hence used another variable finalFee
