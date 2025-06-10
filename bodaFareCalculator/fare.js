c0a17d8aa481677b18bf549431853678529d7ac9
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-06-10 18:31:40 +0300

    Correct pikipiki fare solution

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,27 +1,27 @@
-function calculateBodaFare(distanceInKm) {
+function calculateBodaFare() {
+    const inputDistance = prompt("Unafika wapi Mkubwa? Kilometer ngapi?:");
+    const distance = parseInt(inputDistance);
+
+    // Validate input
+    if (isNaN(distance) || distance <= 0) {
+        alert(`Please enter a vaid number!`)
+        return;
+    }
+
     const baseFare = 50;   // Base fare in Ksh(Added every time)
     const chargePerKm = 15; // Charge per kilometer in Ksh
 
     // Calculate distance charge
-    const distanceCharge = distanceInKm * chargePerKm;
+    const totalCharge = distance * chargePerKm;
 
     // Calculate the total fare
-    const totalFare = baseFare + (chargePerKm * distanceInKm); 
-    // const totalFare = baseFare + distanceCharge; // Alternative way to calculate total fare
+    const totalFare = baseFare + totalCharge;
 
-console.log(`Uko kwote? Io ni ${distanceInKm} km.`);
-console.log(`Ukikalia pikipiki: KES ${baseFare}`);
-console.log(`Mpaka uko: KES ${distanceCharge}`);
-console.log(`Total KES ${totalFare}`);
+    // Message to output
+    alert(`Uko kwote? Io ni ${distance} km.
+        Ukikalia pikipiki: KES ${baseFare}
+        Mpaka uko: KES ${totalCharge}
+        \n Total: KES ${totalFare}
+        `);
 }
-
-// Prompt the user for the distance in kilometers
-const inputDistance = prompt("Unafika wapi Mkubwa? Kilometer ngapi?:");
-const distance = Number(inputDistance);
-
-// Validate the input and call the function
-if (isNaN(distance) || distance <= 0) {
-    console.log("Please enter a valid distance in kilometers.");
-} else {
-    calculateBodaFare(distance);
-}
+calculateBodaFare()
